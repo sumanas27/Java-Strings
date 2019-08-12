61bc85bc0347e7dfe93282cbfdd32b4fdfa86269
Sumana Saha <34478920+sumanas27@users.noreply.github.com>
2019-08-12 16:19:22 +0200

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        int length = A.length()+B.length();
+        System.out.println(length);
+
+        if(A.compareTo(B) < 0) {
+            System.out.println("No");
+        }
+        String firstLetterA = A.substring(0,1).toUpperCase();
+        String restLettersA = A.substring(1).toLowerCase();
+
+        String firstLetterB = B.substring(0,1).toUpperCase();
+        String restLettersB = B.substring(1).toLowerCase();
+        System.out.println(firstLetterA + restLettersA + " " + firstLetterB + restLettersB);
+        
+    }
+}
+
+
+
